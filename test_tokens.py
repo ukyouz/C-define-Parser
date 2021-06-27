e9237239558175415f984364890b2d0e1d1056b1
ukyouz <itainan.101@gmail.com>
2021-06-27 17:22:54 +0800

    switch to py.test for unittest
    Signed-off-by: ukyouz <itainan.101@gmail.com>

--- test_tokens.py
+++ test_tokens.py
@@ -1,148 +1,111 @@
-import unittest
+import pytest
 import pathlib
-from C_DefineParser import DEFINE, Parser
+from C_DefineParser import Parser
 
-class TestAllTokens(unittest.TestCase):
+@pytest.fixture
+def parser_default():
+	parser = Parser()
+	inc_path = './samples'
+	for inc_file in pathlib.Path(inc_path).glob('**/*.h'):
+		parser.read_h(inc_file)
+	return parser
 
-	@classmethod
-	def setUpClass(cls):
-		cls.parser = Parser()
-		inc_path = './samples'
-		for inc_file in pathlib.Path(inc_path).glob('**/*.h'):
-			cls.parser.read_h(inc_file)
-		return cls
-	
-	def setUp(self):
-		self.parser.debug = False
-		self.parser.iterate = 0
+@pytest.mark.parametrize("token, expected_value", [
+	('NVME_CMD_SIZE', 64),
+	('0x00C0000', 0x0C0000),
+	('ALIGN_2N(0x00C0000, 0xC00)', 0x0C0000),
+	('MULTI_LINE_DEF', 3),
+	('MULTI_LINE_DEF', 3),
+	('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000),
+	('NVME_IO_QUEUEN_BASE(8)', 0x2D10000)
+])
+def test_token(parser_default, token, expected_value):
+	token = parser_default.expand_token(token)
+	value = eval(token)
+	assert value == expected_value
 
-	def _assert_define(self, name, expected_val):
-		define = self.parser.get_expand_define(name)
-		value = eval(define.token)
-		self.assertEqual(value, expected_val)
-	
-	def _assert_token(self, token, expected_val):
-		token = self.parser.expand_token(token)
-		value = eval(token)
-		self.assertEqual(value, expected_val)
-	
-	def test_simple_token(self):
-		self._assert_token('NVME_CMD_SIZE', 64)
-		self._assert_token('0x00C0000', 0x0C0000)
-		self._assert_token('ALIGN_2N(0x00C0000, 0xC00)', 0x0C0000)
+@pytest.mark.parametrize("define, expected_value", [
+	('BUFFER_ALIGN_SHIFT', 2),
+	('MULTI_LINE_DEF', 3),
+	('BUFFER_0_BASE', 0x4000000),
+	('NVME_IO_QUEUE0_BASE', 0xD10000),
+])
+def test_define(parser_default, define, expected_value):
+	define = parser_default.get_expand_define(define)
+	value = eval(define.token)
+	assert value == expected_value
 
-	def test_simple_define(self):
-		self._assert_define('BUFFER_ALIGN_SHIFT', 2)
+################################################################################
 
-	def test_multiline_token(self):
-		self._assert_token('MULTI_LINE_DEF', 3)
+@pytest.fixture
+def parser_public():
+	parser = Parser()
+	inc_path = './samples'
+	parser.insert_define('ENV', token='ENV_PUBLIC')
+	parser.read_folder_h(inc_path)
+	return parser
 
-	def test_multiline_define(self):
-		self._assert_define('MULTI_LINE_DEF', 3)
-    
-	def test_complex_token(self):
-		self._assert_token('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000)
-		self._assert_token('NVME_IO_QUEUEN_BASE(8)', 0x2D10000)
+@pytest.mark.parametrize("token, expected_value", [
+	('NVME_CMD_SIZE', 64),
+	('BUFFER_ALIGN_SHIFT', 2),
+	('MULTI_LINE_DEF', 3),
+	('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000),
+	('NVME_IO_QUEUEN_BASE(8)', 0x2D10000)
+])
+def test_token(parser_public, token, expected_value):
+	token = parser_public.expand_token(token)
+	value = eval(token)
+	assert value == expected_value
 
-	def test_complex_define(self):
-		self._assert_define('BUFFER_0_BASE', 0x4000000)
-		self._assert_define('NVME_IO_QUEUE0_BASE', 0xD10000)
+@pytest.mark.parametrize("define, expected_value", [
+	('BUFFER_ALIGN_SHIFT', 2),
+	('MULTI_LINE_DEF', 3),
+	('BUFFER_0_BASE', 0x4000000),
+	('NVME_IO_QUEUE0_BASE', 0xD10000),
+])
+def test_define(parser_public, define, expected_value):
+	define = parser_public.get_expand_define(define)
+	value = eval(define.token)
+	assert value == expected_value
 
-class TestENV_PUBLIC_Tokens(unittest.TestCase):
+def test_existence(parser_public):
+	define = parser_public.get_expand_define('BUFFER_1_BASE', try_if_else=True)
+	assert define == None
 
-	@classmethod
-	def setUpClass(cls):
-		cls.parser = Parser()
-		inc_path = './samples'
-		cls.parser.insert_define('ENV', token='ENV_PUBLIC')
-		cls.parser.read_folder_h(inc_path)
-		return cls
-	
-	def setUp(self):
-		self.parser.debug = False
-		self.parser.iterate = 0
+################################################################################
 
-	def _assert_define(self, name, expected_val):
-		define = self.parser.get_expand_define(name, try_if_else=True)
-		value = eval(define.token)
-		self.assertEqual(value, expected_val)
-	
-	def _assert_token(self, token, expected_val):
-		token = self.parser.expand_token(token, try_if_else=True)
-		value = eval(token)
-		self.assertEqual(value, expected_val)
-	
-	def test_simple_token(self):
-		self._assert_token('NVME_CMD_SIZE', 64)
+@pytest.fixture
+def parser_test():
+	parser = Parser()
+	inc_path = './samples'
+	parser.insert_define('ENV', token='ENV_TEST')
+	parser.read_folder_h(inc_path)
+	return parser
 
-	def test_simple_define(self):
-		self._assert_define('BUFFER_ALIGN_SHIFT', 2)
+@pytest.mark.parametrize("token, expected_value", [
+	('NVME_CMD_SIZE', 64),
+	('BUFFER_ALIGN_SHIFT', 2),
+	('MULTI_LINE_DEF', 3),
+	('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000),
+	('NVME_IO_QUEUEN_BASE(8)', 0x2B10000)
+])
+def test_token(parser_test, token, expected_value):
+	token = parser_test.expand_token(token)
+	value = eval(token)
+	assert value == expected_value
 
-	def test_multiline_token(self):
-		self._assert_token('MULTI_LINE_DEF', 3)
+@pytest.mark.parametrize("define, expected_value", [
+	('BUFFER_ALIGN_SHIFT', 2),
+	('MULTI_LINE_DEF', 3),
+	('BUFFER_0_BASE', 0x4000000),
+	('NVME_IO_QUEUE0_BASE', 0xB10000),
+])
+def test_define(parser_test, define, expected_value):
+	define = parser_test.get_expand_define(define)
+	value = eval(define.token)
+	assert value == expected_value
 
-	def test_multiline_define(self):
-		self._assert_define('MULTI_LINE_DEF', 3)
-    
-	def test_complex_token(self):
-		self._assert_token('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000)
-		self._assert_token('NVME_IO_QUEUEN_BASE(8)', 0x2D10000)
+def test_existence(parser_test):
+	define = parser_test.get_expand_define('BUFFER_1_BASE', try_if_else=True)
+	assert define == None
 
-	def test_complex_define(self):
-		self._assert_define('BUFFER_0_BASE', 0x4000000)
-		self._assert_define('NVME_IO_QUEUE0_BASE', 0xD10000)
-	
-	def test_existence_define(self):
-		define = self.parser.get_expand_define('BUFFER_1_BASE', try_if_else=True)
-		self.assertIsNone(define)
-
-class TestENV_PUBLIC_Tokens(unittest.TestCase):
-
-	@classmethod
-	def setUpClass(cls):
-		cls.parser = Parser()
-		inc_path = './samples'
-		cls.parser.insert_define('ENV', token='ENV_TEST')
-		cls.parser.read_folder_h(inc_path)
-		return cls
-	
-	def setUp(self):
-		self.parser.debug = False
-		self.parser.iterate = 0
-
-	def _assert_define(self, name, expected_val):
-		define = self.parser.get_expand_define(name, try_if_else=True)
-		value = eval(define.token)
-		self.assertEqual(value, expected_val)
-	
-	def _assert_token(self, token, expected_val):
-		token = self.parser.expand_token(token, try_if_else=True)
-		value = eval(token)
-		self.assertEqual(value, expected_val)
-	
-	def test_simple_token(self):
-		self._assert_token('NVME_CMD_SIZE', 64)
-
-	def test_simple_define(self):
-		self._assert_define('BUFFER_ALIGN_SHIFT', 2)
-
-	def test_multiline_token(self):
-		self._assert_token('MULTI_LINE_DEF', 3)
-
-	def test_multiline_define(self):
-		self._assert_define('MULTI_LINE_DEF', 3)
-    
-	def test_complex_token(self):
-		self._assert_token('(ALIGN_2N(BUFFER_BASE, BUFFER_ALIGN_SHIFT))', 0x4000000)
-		self._assert_token('NVME_IO_QUEUEN_BASE(8)', 0x2B10000)
-
-	def test_complex_define(self):
-		self._assert_define('BUFFER_0_BASE', 0x4000000)
-		self._assert_define('NVME_IO_QUEUE0_BASE', 0xB10000)
-	
-	def test_existence_define(self):
-		define = self.parser.get_expand_define('BUFFER_1_BASE', try_if_else=True)
-		self.assertIsNone(define)
-
-if __name__ == '__main__':
-    unittest.main()
