1b3c272fafc7417226240ce16911beca3980afbf
Chihhung Cheng <chihhung_cheng@tel.com>
2023-11-10 12:18:57 +0900

    make pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='C_DefineParser',
+    version='0.1.0',
+    description='A example Python package',
+    url='https://github.com/ukyouz/C-define-Parser',
+    author='Johnny Cheng',
+    author_email='zhung1206@gmail.com',
+    license='MIT',
+    py_modules=['C_DefineParser'],
+    install_requires=[],
+)
